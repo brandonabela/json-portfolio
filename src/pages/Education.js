e2541032bdb75a0b928fb356f8a18867ebc83234
Brandon Abela <brandonabela9@gmail.com>
2021-09-22 17:26:58 +0200

    Add Education Page

--- src/pages/Education.js
+++ src/pages/Education.js
@@ -1,14 +1,52 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Badge, Container } from 'react-bootstrap';
 import PageHeader from '../components/PageHeader';
 
+import './Education.scss';
+import educations from '../data/educations.json';
 
 function Education() {
   return (
-    <div>
+    <div className="education content">
       <PageHeader name="Education"></PageHeader>
 
       <Container>
+        <ul className="timeline-vertical">
+          {educations.map((education, index) =>
+            <li className={index % 2 === 0 ? " " : "inverted"} key={index}>
+              <div className="timeline-item">
+
+                <div className="item-header">
+                  <div className="title-time">
+                    <h3 className="title"> {education.course} </h3>
+
+                    <div className="time"> {education.start + " - " + education.end} </div>
+                  </div>
+
+                  <div className="location"> {education.institute + ", " + education.location} </div>
+                </div>
+
+                {education.description.length > 0 &&
+                  <p className="description"> {education.description} </p>
+                }
+
+                {education.bullets.length > 0 &&
+                  <ul className={(Math.max(...education.bullets.map(x => x.split().length)) > 20 ? "" : "two-column-bullets")}>
+                    {education.bullets.map((bullet, index) =>
+                      <li key={index}>
+                        {bullet}
+                      </li>
+                    )}
+                  </ul>
+                }
+
+                {education.technologies.length > 0 && education.technologies.map((technology, index) =>
+                  <Badge pill key={index}> {technology} </Badge>
+                )}
+              </div>
+            </li>
+          )}
+        </ul>
       </Container>
     </div>
   );
