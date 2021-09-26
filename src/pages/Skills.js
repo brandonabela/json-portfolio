fb1293f1c6c56e64e41f87efb90b67c7d6b65eb8
Brandon Abela <brandonabela9@gmail.com>
2021-09-26 23:27:11 +0200

    Add Skills Page

--- src/pages/Skills.js
+++ src/pages/Skills.js
@@ -1,16 +1,40 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Badge, Col, Container, Row } from 'react-bootstrap';
 import PageHeader from '../components/PageHeader';
+
+import './Skills.scss';
+import skills from '../data/skills.json';
 
 
 function Skills() {
   return (
-    <div>
+    <div className="skills content">
       <PageHeader name="Skills"></PageHeader>
 
-      <Container>
-      </Container>
-    </div>
+      <section className='section-padding'>
+        <Container>
+          <Row>
+            {skills.map((skill, index) =>
+              <Col sm={4} key={index}>
+                <div className="skill-item">
+                  {skill.icon &&
+                    <div className="skill-icon text-center"> <i className={'bi ' + skill.icon}></i> </div>
+                  }
+
+                  <h5 style={{ marginTop: skill.icon ? '-2rem' : '0' }}> {skill.title} </h5>
+
+                  <ul>
+                    {skill.skills.map((skill, s_index) =>
+                      <Badge pill key={s_index}> {skill} </Badge>
+                    )}
+                  </ul>
+                </div>
+              </Col>
+            )}
+          </Row>
+        </Container>
+      </section>
+    </div >
   );
 }
 
