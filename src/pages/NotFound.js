f388894c05f59185842ec3a120c07c240c85dc6f
Brandon Abela <brandonabela9@gmail.com>
2021-09-29 23:45:22 +0200

    Add Not Found Page

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,9 +1,24 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+import profile from '../data/profile.json';
+import PageHeader from '../components/PageHeader';
+
 
 function NotFound() {
   return (
-    <div>
-      <h2>Not Found</h2>
+    <div className="notFound content">
+      <PageHeader name="Page Not Found"></PageHeader>
+
+      <section className='section-padding'>
+        <Container>
+          <p className="content-gutter text-center"> {profile.notFound} </p>
+
+          <div className="spacer" />
+          <Link className="btn" role="button" to="/"> Return Home </Link>
+        </Container>
+      </section>
     </div>
   );
 }
